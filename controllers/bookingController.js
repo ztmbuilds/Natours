5df3802b8b4b0ee4ddb258baf9a5c3d40bcd0fba
Zetro <s.zetro007@gmail.com>
2022-12-12 02:19:02 +0100

    Updated bookingController.js

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -53,6 +53,6 @@ exports.createBookingCheckout = catchAsync(async (req, res, next) => {
 
 exports.createBooking = factory.createOne(Booking);
 exports.getBooking = factory.getOne(Booking);
-exports.getAllBookings = factory.getAll.getOne(Booking);
-exports.updateBooking = factory.updateOne.getOne(Booking);
-exports.deleteBooking = factory.deleteOne.getOne(Booking);
+exports.getAllBookings = factory.getAll(Booking);
+exports.updateBooking = factory.updateOne(Booking);
+exports.deleteBooking = factory.deleteOne(Booking);
